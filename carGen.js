d58eb26d142e8dc074e59a98e3f43af309188d55
Your Name <you@example.com>
2022-12-16 10:58:53 -0700

    web page added, css added, included hex colors for cars to change font color.

--- carGen.js
+++ carGen.js
@@ -2,10 +2,12 @@ const cars = ( brand) => {
     return {
         brand: {
         'Acura': {model:['MDX', 'NSX', 'TSX'],
-                  color:['Apex Blue Pearl', 'Lunar Silver Metallic', 'Majestic Black Pearl', 'Performance Red Pearl', 'Platinum White Pearl']},
+                  color:['Apex Blue Pearl', 'Lunar Silver Metallic', 'Majestic Black Pearl', 'Performance Red Pearl', 'Platinum White Pearl'],
+                  hexColor:['#00039C', '#aaa9ad', '#000000', '#591214', '#ffffff']},
       'Bentley':{
         model: ['Continental GT', 'Flying Spur', 'Bentayga'],  
-        color: ['Oxford Blue', 'Extreme Silver', 'Black Crystal', 'St. James Red', 'Ghost White'],}, 
+        color: ['Oxford Blue', 'Extreme Silver', 'Black Crystal', 'St. James Red', 'Ghost White'],
+        hexColor: ['#002147', '#A8A9AD', '#000000', '#B62232', '#ffffff'],}, 
     }
 }
         
@@ -18,7 +20,12 @@ const cars = ( brand) => {
     var acuraModels = cars().brand['Acura'].model
     var bentleyModels = cars().brand['Bentley'].model
     let brandCount= Object.keys(cars().brand).length
+
+    //colors to hex values
+   
     
+
+
 const randomizer = () => {
   let b = Math.floor(Math.random() * brandCount)
   let aM = Math.floor(Math.random() * acuraModels.length)
@@ -32,18 +39,32 @@ const randomizer = () => {
 //console.log(rCB)
 
     if (Object.keys(cars().brand)[b] === 'Acura') {
-        return cars().brand['Acura'], cars().brand['Acura'].model[aM], cars().brand['Acura'].color[rCA],
+        return cars().brand['Acura'], 
+        cars().brand['Acura'].model[aM], 
+        cars().brand['Acura'].color[rCA], 
+        document.getElementById("brand").innerHTML = "Acura",
+        document.getElementById("model").innerHTML = cars().brand['Acura'].model[bM],
+        document.getElementById("color").innerHTML = cars().brand['Acura'].color[rCB],
+        document.getElementById("container").style.color = cars().brand['Acura'].hexColor[rCB],
         console.log('Brand: ' + Object.keys(cars().brand)[b] + ' Model: ' + cars().brand['Acura'].model[aM] + ' color: ' +  cars().brand['Acura'].color[rCA])
       }  
     if (Object.keys(cars().brand)[b] === 'Bentley') {
-    return cars().brand['Bentley'], cars().brand['Bentley'].model[bM], cars().brand['Bentley'].color[rCB],
+    return cars().brand['Bentley'], 
+    cars().brand['Bentley'].model[bM], 
+    cars().brand['Bentley'].color[rCB], 
+    document.getElementById("brand").innerHTML = "Bentley",
+    document.getElementById("model").innerHTML =  cars().brand['Bentley'].model[bM],
+    document.getElementById("color").innerHTML =  cars().brand['Bentley'].color[rCB],
+    document.getElementById("container").style.color = cars().brand['Bentley'].hexColor[rCB], 
     console.log('Brand: ' + Object.keys(cars().brand)[b] + ' Model: ' + cars().brand['Bentley'].model[bM]+ ' color: ' + cars().brand['Bentley'].color[rCB])
   } 
 }
 
 
-console.log('What car should you buy?')
+console.log('Random Car Generator')
   console.log(randomizer())   
+
+
 
 
 
